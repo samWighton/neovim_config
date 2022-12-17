7b482e2063d650f9f84012c4cad273815d0f0a9e
Sam Wighton <sam@samuelwighton.com>
2022-12-17 18:15:07 +1100

    Added nvim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,14 @@
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'chriskempson/base16-vim'
+    use 'airblade/vim-gitgutter'
+    use 'dense-analysis/ale'
+    use 'HerringtonDarkholme/yats.vim'
+    use 'othree/html5.vim'
+    use 'pangloss/vim-javascript'
+    use 'evanleck/vim-svelte'
+end)
+
+-- use 'chriskempson/base16-vim'
+-- use 'Soares/base16.nvim'
+
